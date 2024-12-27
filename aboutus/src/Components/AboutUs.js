5c5fefcce8532e55db621db0fd73811ce8ceca58
Abhishek-tech471 <abhishek.yadav_cs.bct23@gla.ac.in>
2024-12-27 17:25:55 +0530

    commit 3

--- aboutus/src/Components/AboutUs.js
+++ aboutus/src/Components/AboutUs.js
@@ -8,22 +8,31 @@ import image3 from '../Images/image3.jpg'
 export default function AboutUs() {
   return (
     <div className="main">
-      <h1>ABOUT US</h1>
+          <div className="C">ABOUT US</div>
       <div className="content">
         {/* This is text */}
         <div className="text">
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere fugit eligendi odit fuga autem fugiat error blanditiis animi repellendus nam ex at numquam quisquam doloremque expedita nisi, quis recusandae, inventore reprehenderit. Quia dolore debitis culpa natus blanditiis beatae. Deserunt veritatis dolorem explicabo possimus tenetur temporibus!
         </div>
         {/* This is for images */}
-        <div className="image">
+        {/* <div className="image">
            <img id="img1"  />
            <img id="img2" src={image2}alt="Image 2" />
            <img id="img3" src={image3} alt="Image 3" />
            <img id="img4" src={image1} alt="Image 4" />
-        </div>
+        </div> */}
+        <div class="image">
+  <img id="img1" src={image1} alt="Image 1" />
+  <img id="img2" src={image2} alt="Image 2" />
+</div>
+
         
       </div>
-      
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
 
       {/* next part */}
 
@@ -32,8 +41,6 @@ export default function AboutUs() {
         {/* This is for images */}
         <div className="image">
            
-           <img id="img2" src={image2}alt="Image 2" />
-           <img id="img1"  />
            <img id="img3" src={image3} alt="Image 3" />
            <img id="img4" src={image1} alt="Image 4" />
         </div>
@@ -47,6 +54,9 @@ export default function AboutUs() {
             <img src="" alt="" />
         </div>
       </div>
+      <br />
+      <br />
+      <br />
 
     </div>
   )
